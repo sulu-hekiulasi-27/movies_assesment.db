63f02a0a960fa455424a611f9554cc9337135886
sulu-hekiulasi-27 <suluhekiulasi@riccarton.school.nz>
2024-07-04 09:35:12 +1200

    Created a new file for my Python user interface

---
+++ movies_database.py
@@ -0,0 +1,24 @@
+# Import the libraries to connect to the database and present the information in tables
+import sqlite3
+from tabulate import tabulate
+
+# This is the filename of the database to be used
+DB_NAME = 'movies_database.db'
+
+def print_query(view_name:str):
+    ''' Prints the specified view from the database in a table '''
+    # Set up the connection to the database
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    # Get the results from the view
+    sql = "SELECT * FROM '" + view_name + "'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # Get the field names to use as headings
+    field_names = "SELECT name from pragma_table_info('" + view_name + "') AS tblInfo"
+    cursor.execute(field_names)
+    headings = list(sum(cursor.fetchall(),()))
+    # Print the results in a table with the headings
+    print(tabulate(results,headings))
+    db.close()
+    
