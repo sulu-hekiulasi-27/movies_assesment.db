432e8e162637a55c809737143092379f84792097
sulu-hekiulasi-27 <suluhekiulasi@riccarton.school.nz>
2024-07-04 10:48:26 +1200

    I ran the code for my python query for movied_database.py

--- movies_database.py
+++ movies_database.py
@@ -23,7 +23,8 @@ def print_query(view_name:str):
     db.close()
     
     menu_choice =''
-    while menu_choice = input('Welcome to the Movies database\n\n'
+    while menu_choice != 'Z':
+        menu_choice = input('Welcome to the Movies database\n\n'
                               'Type the letter for the information you want:\n'
                               'A: All movies made in Japan\n '
                               'B: title, director & studio of movies made by Disney and Pixar\n'
@@ -31,5 +32,26 @@ def print_query(view_name:str):
                               'D: Title and ratings of movies made by Warner Bros\n'
                               'E: title, country & studio of all movies made in NZ\n'
                               'F: title ratings & release date for all movies\n'
-                              'G: title, studio & director of')
-                              
+                              'G: title, studio, director & ratings of all movies made by Disney\n'
+                              'H: all information for movies made by Illumination Entertainment\n'
+                              'I: all movies released in the 2000s'
+                              'Z: Exit\n\nType option here: ')
+        menu_choice = menu_choice.upper()
+        if menu_choice == 'A':
+            print_query('Movies made In Japan')
+        elif menu_choice == 'B':
+            print_query('Disney & Pixar movies')
+        elif menu_choice == 'C':
+            print_query('Released in the 90s')
+        elif menu_choice == 'D':
+            print_query('Warner Bros movies')
+        elif menu_choice == 'E':
+            print_query('Movies made in NZ')
+        elif menu_choice == 'F':
+            print_query('title, ratings & release date for all movies')
+        elif menu_choice == 'G':
+            print_query('Movies made by Walt Disney Studios')
+        elif menu_choice == 'H':
+            print_query('Illumination Entertainment All Information')
+        elif menu_choice == 'I':
+            print_query('All movies released in the 2000s')
